61ae1e9ebfb99ff5817fceda4242e25de00519e2
Johannes <johannesagdonald@gmail.com>
2024-12-28 13:38:46 +0100

    Clean up 24 for possible part 2

--- AdventOfCode2024Kotlin/src/main/kotlin/org/johdonald/aoc2024/Main.kt
+++ AdventOfCode2024Kotlin/src/main/kotlin/org/johdonald/aoc2024/Main.kt
@@ -1,6 +1,6 @@
 package org.johdonald.aoc2024
 
-import org.johdonald.aoc2024.day24.part1
+import org.johdonald.aoc2024.day24.*
 
 fun main() {
     println(part1())
--- AdventOfCode2024Kotlin/src/main/kotlin/org/johdonald/aoc2024/day24/Day24.kt
+++ AdventOfCode2024Kotlin/src/main/kotlin/org/johdonald/aoc2024/day24/Day24.kt
@@ -1,6 +1,11 @@
 package org.johdonald.aoc2024.day24
 
 import org.johdonald.aoc2024.utils.readInput
+import org.johdonald.aoc2024.day24.operations
+
+fun operations(input1: String, input2: String, op: String): String {
+    return "($input1 $op $input2)"
+}
 
 val operators: Map<String, (Int, Int) -> Int> = mapOf(
     "AND" to { a, b -> a and b },
@@ -8,29 +13,51 @@ val operators: Map<String, (Int, Int) -> Int> = mapOf(
     "OR" to { a, b -> a or b }
 )
 
-fun part1(): Long {
-    val (wires, gates) = readInput("day24").split("\n\n")
-    val wireValueMap = wires.split("\n").map { it.split(": ") }.associate { (k, v) -> k to v.toInt() }.toMutableMap().toSortedMap()
-    val gateOperations = gates.split("\n").map { it.split(" ") }.toMutableList()
-    var i = 0
-    while (gateOperations.size > 0) {
-        val (input1, operator, input2, _, output) = gateOperations[i]
-        if (input1 in wireValueMap && input2 in wireValueMap && output !in wireValueMap) {
-            wireValueMap[output] = operators.getValue(operator).invoke(wireValueMap[input1]!!, wireValueMap[input2]!!)
-            gateOperations.removeAt(i)
-        }
-        i = if (i >= gateOperations.size - 1) 0 else i + 1
-    }
-    val binaryValue = getBinaryValue(wireValueMap, 'z')
-    return binaryValue.toLong(2)
+fun generateXyValueMap(): Map<String, Int> {
+    val (xyValues, _) = readInput("day24").split("\n\n")
+    val xyValueMap = xyValues.split("\n").map { it.split(": ") }.associate { (k, v) -> k to v.toInt() }.toMutableMap()
+    return xyValueMap
 }
 
-fun getBinaryValue(wireValueMap: Map<String, Int>, char: Char): String {
+fun generateGatesMap(): Map<String, List<String>> {
+    val (_, gates) = readInput("day24").split("\n\n")
+    val gateOperations = gates.split("\n").map { it.split(" -> ") }.associate { (v, k) -> k to v.split(" ") }.toMutableMap()
+    return gateOperations
+}
+
+fun runCircuit(xyValueMap: Map<String, Int>, gatesMap: Map<String, List<String>>): Map<String, Int> {
+    val outputsMap: MutableMap<String, Int> = xyValueMap.toMutableMap().toSortedMap()
+    val gatesKeys = gatesMap.keys.toMutableList()
+    var i = 0
+    while (gatesKeys.size > 0) {
+        val output = gatesKeys[i]
+        val (input1, operator, input2) = gatesMap[output]!!
+        if (input1 in outputsMap && input2 in outputsMap && output !in outputsMap) {
+            outputsMap[output] = operators[operator]!!(outputsMap[input1]!!, outputsMap[input2]!!)           
+            gatesKeys.removeAt(i)
+        }
+        i = if (i >= gatesKeys.size - 1) 0 else i + 1
+    }
+    return outputsMap
+}
+
+fun calculateValue(char: Char, valueMap: Map<String, Int>): Long {
+    return getBinaryValue(valueMap, char).toLong(2)
+}
+
+fun getBinaryValue(valueMap: Map<String, Int>, char: Char): String {
     val binaryValue = StringBuilder()
-    for ((k, v) in wireValueMap) {
+    for ((k, v) in valueMap) {
         if (k.first() == char) {
             binaryValue.insert(0, v)
         }
     }
     return binaryValue.toString()
+}
+
+fun part1(): Long {
+    val xyValueMap = generateXyValueMap()
+    val gatesMap = generateGatesMap()
+    val outputsMap = runCircuit(xyValueMap, gatesMap)
+    return calculateValue('z', outputsMap)
 }
